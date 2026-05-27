b2dcaf1be806183ed48d1e8cf7952d1c2f1ae0fd
Clonuss152 <ddonath@web.de>
2026-05-27 10:54:11 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Core Satellite System",
+    layout="wide"
+)
+
+st.title("Core + Satellite Trading System")
+
+st.success("System läuft erfolgreich.")
+
+st.write("Die erste Version der App ist online.")
