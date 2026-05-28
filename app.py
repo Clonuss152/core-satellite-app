bc7bdee99c2fd03d4edcd4c3999bd2f1a3779cc2
Clonuss152 <ddonath@web.de>
2026-05-28 11:16:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from supabase import create_client
 from datetime import date
-
+from metadata_enrichment import enrich_underlyings
 from config import CORE_TICKERS, SAT_TICKERS
 from utils import add_business_days
 from daily_update import run_daily_update
