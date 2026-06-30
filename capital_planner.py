0ec1daa365da9f96fe3df642b2872a70c698f0ce
Clonuss152 <ddonath@web.de>
2026-06-30 12:27:50 +0200

    Update capital_planner.py

--- capital_planner.py
+++ capital_planner.py
@@ -442,6 +442,9 @@ def calculate_capital_plan(
     if not live_value_result["live_values_complete"]:
         buy_block_reason = "LIVE_VALUES_STALE"
 
+    elif sell_orders_present:
+        buy_block_reason = "SELL_ORDERS_PENDING"
+
     elif broker_cash <= 0:
         buy_block_reason = "INSUFFICIENT_BROKER_CASH"
         
